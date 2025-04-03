df4651d6e3e6b31115899d5778befb33e72c773e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-03 22:10:24 +0530

    Removed DataService

--- src/app/tabs/home/home.component.ts
+++ src/app/tabs/home/home.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core'
-
-import { DataService, DataItem } from '../shared/data.service'
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'Home',
-  templateUrl: './home.component.html',
+  selector: "Home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-  items: Array<DataItem>
+  constructor() {}
 
-  constructor(private _itemService: DataService) {}
-
-  ngOnInit(): void {
-    this.items = this._itemService.getItems()
-  }
+  ngOnInit(): void {}
 }
