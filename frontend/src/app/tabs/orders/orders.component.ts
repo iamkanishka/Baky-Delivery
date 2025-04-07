539db7fce32400afa9d2c2466a28bb6cf771e79c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-07 23:53:51 +0530

    chore: Hide Status Bar

--- frontend/src/app/tabs/orders/orders.component.ts
+++ frontend/src/app/tabs/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-orders',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './orders.component.scss'
 })
 export class OrdersComponent {
+  constructor(private _page: Page) {
+    this._page.actionBarHidden = true;
+  }
 
 }
