27bef5c62e5df407c84f6f2aaa80feed0d325342
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-21 21:35:07 +0530

    Added Auth, Orders Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
+  imports: [AuthModule, OrdersModule],
 })
 export class AppModule {}
