1765fd0860335a32a114d98541836fcd5f2d8be1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-22 21:30:50 +0530

    Added Noitification and Search Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
+import { NotificationModule } from './notification/notification.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
-  imports: [AuthModule, OrdersModule],
+  imports: [AuthModule, OrdersModule, NotificationModule, SearchModule],
 })
 export class AppModule {}
